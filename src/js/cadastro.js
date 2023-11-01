db2c41810236962df928aa71e5f2e3384b3a8b8d
RafaelPulzi <rafaelpulzi.profissional@gmail.com>
2023-11-01 01:51:38 -0300

    Cadastro funciona perfeitamente

--- src/js/cadastro.js
+++ src/js/cadastro.js
@@ -1,49 +1,35 @@
 function submitCadastro(event) {
-  event.preventDefault();
+  event.preventDefault(); // Impede o envio do formulário por padrão
 
-  fetch("http://localhost:9090/authentication/registerUsuario", {
-      method: "POST",
+  fetch("http://localhost:9090/authentication/registerCliente", {
+      method: 'POST',
       headers: {
         'Accept': 'application/json',
         'Content-Type': 'application/json'
       },
       body: JSON.stringify({
-        emailUsuario: document.getElementById('email').value,
-        senha_usuario: document.getElementById('senha').value,
+        nm_cliente: document.getElementById("nome").value,
+        cpfCliente: document.getElementById("cpf").value,
+        logradouro_cliente: document.getElementById("logradouro").value,
+        numero_casa_cliente: document.getElementById("numero").value,
+        complemento_cliente: document.getElementById("complemento").value,
+        cidade_cliente: document.getElementById("cidade").value,
+        estado_cliente: document.getElementById("estado").value,
+        usuario: {
+          emailUsuario: document.getElementById("email").value,
+          senha_usuario: document.getElementById("senha").value
+        }
       })
     })
     .then(response => response.json())
     .then(data => {
-      const id_usuario_cliente = data.id_usuario; // ID do usuário cliente gerado pelo servidor
-
-      return fetch("http://localhost:9090/authentication/registerCliente", {
-        method: "POST",
-        headers: {
-          'Accept': 'application/json',
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-          nm_cliente: document.getElementById('nome').value,
-          cpfCliente: document.getElementById('cpf').value,
-          logradouro_cliente: document.getElementById('logradouro').value,
-          complemento_cliente: document.getElementById('complemento').value,
-          estado_cliente: document.getElementById('estado').value,
-          cidade_cliente: document.getElementById('cidade').value,
-          numero_casa_cliente: document.getElementById('numero').value,
-          id_usuario_cliente: id_usuario_cliente
-        })
-      });
-    })
-    .then(response => response.json())
-    .then(data => {
-      console.log(data);
+      console.log('Sucesso:', data);
       window.location.href = 'inicio.html';
     })
     .catch(error => {
-      console.error('Erro ao criar usuário:', error);
+      console.error('Erro:', error);
     });
 }
-
 const botao = document.getElementById('concluir');
 botao.addEventListener('click', function (event) {
   event.preventDefault();
